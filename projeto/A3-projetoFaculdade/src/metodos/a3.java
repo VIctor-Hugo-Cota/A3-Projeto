5dc688146033328bd132e1695ee24a1622af9b03
VIctor-Hugo-Cota <143911839+VIctor-Hugo-Cota@users.noreply.github.com>
2026-05-12 17:26:50 -0300

    ultima atualização
    classe e array configurado

--- projeto/A3-projetoFaculdade/src/metodos/a3.java
+++ projeto/A3-projetoFaculdade/src/metodos/a3.java
@@ -14,13 +14,13 @@ public class a3 {
         while (ativo == true) {
             LimparTela.limpar();
             System.out.println("=============== Folha de pagamentos ===============");
-            System.out.println("1 - Cadastrar colaborador");
-            System.out.println("2 - Listar colaboradores");
-            System.out.println("3 - Sair");
+            System.out.println("1 - Cadastrar colaborador ");
+            System.out.println("2 - Listar colaboradores ");
+            System.out.println("3 - Sair ");
             System.out.println("===================================================");
 
             int opcao1 = sc.nextInt();
-
+            sc.nextLine();
             switch (opcao1) {
                 case 1:
                     // cadastra um novo colaborador
@@ -31,12 +31,13 @@ public class a3 {
                         System.out.println("1 - Funcionário padrão");
                         System.out.println("2 - Funcionário comissionado");
                         System.out.println("3 - Funcionário de produção");
-                        System.out.println("4 - Sair");
+                        System.out.println("4 - Voltar ");
                         System.out.println("==========================================");
                         int opcao2 = sc.nextInt();
+                        sc.nextLine();
 
                         switch (opcao2) {
-                            case 1:
+                            case 1: {
                                 // padrão (somente salario base)
                                 LimparTela.limpar();
                                 System.out.println("Digite o nome completo do Funcionário:");
@@ -44,12 +45,18 @@ public class a3 {
 
                                 System.out.println("Digite seu numero de registro:");
                                 int registro = sc.nextInt();
+                                sc.nextLine();
 
                                 Double salario = 2000.00;
 
+                                Funcionario f = new Funcionario(nome, registro, salario);
+
+                                listaFuncionarios.add(f);
+
                                 System.out.println("Funcionário cadastrado!");
                                 break;
-                            case 2:
+                            }
+                            case 2: {
                                 // comissionado (vendas mes * percentual de comissão / 100)
                                 LimparTela.limpar();
                                 System.out.println("Digite o nome completo do Funcionário:");
@@ -57,19 +64,25 @@ public class a3 {
 
                                 System.out.println("Digite seu numero de registro:");
                                 int registro = sc.nextInt();
+                                sc.nextLine();
 
                                 System.out.println("Qual a quantidade de vendas feitas no ultimo mês?");
                                 int vendas = sc.nextInt();
+                                sc.nextLine();
 
                                 System.out.println("Qual o percentual de comissão?");
                                 Double comissao = sc.nextDouble();
+                                sc.nextLine();
 
                                 Double salarioB = 2000.00;
-                                Double salarioFC = salarioB + ((vendas + comissao) / 100);
+                                Double salarioFC = salarioB + ((vendas * comissao) / 100);
 
-                                System.out.println("Funcionário cadastrado.");
+                                Funcionario f = new Funcionario(nome, registro, salarioFC);
+
+                                listaFuncionarios.add(f);
                                 break;
-                            case 3:
+                            }
+                            case 3: {
                                 // produção (valor da peça produzida * quantidade)
                                 LimparTela.limpar();
                                 System.out.println("Digite o nome completo do Funcionário:");
@@ -77,18 +90,24 @@ public class a3 {
 
                                 System.out.println("Digite seu numero de registro:");
                                 int registro = sc.nextInt();
+                                sc.nextLine();
 
                                 System.out.println("Qual o valor da peça produzida pelo funcionario?");
                                 Double valorp = sc.nextDouble();
+                                sc.nextLine();
 
                                 System.out.println("Qual a quantidade de peças produzidas no ultimo mês?");
                                 int pecas = sc.nextInt();
+                                sc.nextLine();
 
                                 double salarioB = 2000.00;
                                 double salarioFP = salarioB + (valorp * pecas);
 
-                                System.out.println("Funcionário cadastrado.");
+                                Funcionario f = new Funcionario(nome, registro, salarioFP);
+
+                                listaFuncionarios.add(f);
                                 break;
+                            }
                             case 4:
                                 ativo1 = false;
                                 break;
@@ -107,10 +126,12 @@ public class a3 {
 
                         System.out.println("===============");
                     }
+                    System.out.println("Pressione ENTER para continuar...");
+                    sc.nextLine();
                     break;
                 case 3:
                     // fecha o programa
-                    System.out.print("Sistema encerrado");
+                    System.out.print("❌❌❌❌Sistema encerrado❌❌❌❌");
                     ativo = false;
                     break;
 
