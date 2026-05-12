03af62e076dd0c7ca2ed8ad8ba2b65212990ddd7
VIctor-Hugo-Cota <143911839+VIctor-Hugo-Cota@users.noreply.github.com>
2026-05-11 22:08:12 -0300

    arrumando o sistema base(sem armazenamento de dados ainda)

---
+++ projeto/A3-projetoFaculdade/src/metodos/Funcionario.java
@@ -0,0 +1,14 @@
+package metodos;
+
+public class Funcionario {
+
+    String nome;
+    int registro;
+    double salario;
+
+    public Funcionario(String nome, int registro, double salario) {
+        this.nome = nome;
+        this.registro = registro;
+        this.salario = salario;
+    }
+}
--- projeto/A3-projetoFaculdade/src/metodos/a3.java
+++ projeto/A3-projetoFaculdade/src/metodos/a3.java
@@ -1,18 +1,18 @@
 package metodos;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class a3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         LimparTela limpar = new LimparTela();
-        FuncB FB = new FuncB();
-        FuncC FC = new FuncC();
-        FuncP FP = new FuncP();
+        ArrayList<Funcionario> listaFuncionarios = new ArrayList<>();
+
         Boolean ativo = true;
-        
+
         while (ativo == true) {
-             LimparTela.limpar();
+            LimparTela.limpar();
             System.out.println("=============== Folha de pagamentos ===============");
             System.out.println("1 - Cadastrar colaborador");
             System.out.println("2 - Listar colaboradores");
@@ -20,51 +20,93 @@ public class a3 {
             System.out.println("===================================================");
 
             int opcao1 = sc.nextInt();
-            
+
             switch (opcao1) {
                 case 1:
                     // cadastra um novo colaborador
                     Boolean ativo1 = true;
                     while (ativo1 == true) {
-                    LimparTela.limpar();
-                    System.out.println("=============== Coladores ===============");
-                    System.out.println("1 - Funcionário padrão");
-                    System.out.println("2 - Funcionário comissionado");
-                    System.out.println("3 - Funcionário de produção");
-                    System.out.println("4 - Sair");
-                    System.out.println("==========================================");
-                    int opcao2 = sc.nextInt();
+                        LimparTela.limpar();
+                        System.out.println("=============== Coladores ===============");
+                        System.out.println("1 - Funcionário padrão");
+                        System.out.println("2 - Funcionário comissionado");
+                        System.out.println("3 - Funcionário de produção");
+                        System.out.println("4 - Sair");
+                        System.out.println("==========================================");
+                        int opcao2 = sc.nextInt();
 
-                    switch (opcao2) {
-                        case 1:
-                            // padrão (somente salario base)
-                            LimparTela.limpar();
-                            FB.FuncB();
-                            break;
-                        case 2:
-                            // comissionado (vendas mes * percentual de comissão / 100)
-                            LimparTela.limpar();
-                            FC.FuncC();
-                            
-                            break;
-                        case 3:
-                            // produção (valor da peça produzida * quantidade)
-                            LimparTela.limpar();
-                            FP.FuncP();
-                            
-                            break;
-                        case 4:
-                            ativo1 = false;
-                            break;
-                        default:
-                            break;
+                        switch (opcao2) {
+                            case 1:
+                                // padrão (somente salario base)
+                                LimparTela.limpar();
+                                System.out.println("Digite o nome completo do Funcionário:");
+                                String nome = sc.nextLine();
+
+                                System.out.println("Digite seu numero de registro:");
+                                int registro = sc.nextInt();
+
+                                Double salario = 2000.00;
+
+                                System.out.println("Funcionário cadastrado!");
+                                break;
+                            case 2:
+                                // comissionado (vendas mes * percentual de comissão / 100)
+                                LimparTela.limpar();
+                                System.out.println("Digite o nome completo do Funcionário:");
+                                String nome = sc.nextLine();
+
+                                System.out.println("Digite seu numero de registro:");
+                                int registro = sc.nextInt();
+
+                                System.out.println("Qual a quantidade de vendas feitas no ultimo mês?");
+                                int vendas = sc.nextInt();
+
+                                System.out.println("Qual o percentual de comissão?");
+                                Double comissao = sc.nextDouble();
+
+                                Double salarioB = 2000.00;
+                                Double salarioFC = salarioB + ((vendas + comissao) / 100);
+
+                                System.out.println("Funcionário cadastrado.");
+                                break;
+                            case 3:
+                                // produção (valor da peça produzida * quantidade)
+                                LimparTela.limpar();
+                                System.out.println("Digite o nome completo do Funcionário:");
+                                String nome = sc.nextLine();
+
+                                System.out.println("Digite seu numero de registro:");
+                                int registro = sc.nextInt();
+
+                                System.out.println("Qual o valor da peça produzida pelo funcionario?");
+                                Double valorp = sc.nextDouble();
+
+                                System.out.println("Qual a quantidade de peças produzidas no ultimo mês?");
+                                int pecas = sc.nextInt();
+
+                                double salarioB = 2000.00;
+                                double salarioFP = salarioB + (valorp * pecas);
+
+                                System.out.println("Funcionário cadastrado.");
+                                break;
+                            case 4:
+                                ativo1 = false;
+                                break;
+                            default:
+                                break;
+                        }
                     }
-                }
                     break;
                 case 2:
                     // mostra o array lista de colaboradores
-                    LimparTela.limpar();
+                    for (Funcionario f : listaFuncionarios) {
 
+                        System.out.println("Nome: " + f.nome);
+                        System.out.println("Registro: " + f.registro);
+                        System.out.println("Salário: " + f.salario);
+
+                        System.out.println("===============");
+                    }
                     break;
                 case 3:
                     // fecha o programa
